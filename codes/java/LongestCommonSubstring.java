1cfa7b2b6d282d50fed240335910879d5992f930
Rishita <85801090+RishitaMitra@users.noreply.github.com>
2022-10-24 12:56:27 +0530

    update LCS by using Naive recursion
    I used Naive recursion to implement the longest common substring from a string.

--- codes/java/LongestCommonSubstring.java
+++ codes/java/LongestCommonSubstring.java
@@ -1,55 +1,37 @@
-/*
-Given two strings ‘X’ and ‘Y’, find the length of the longest common substring. 
+/* A Naive recursive implementation of LCS problem in java*/
+public class LongestCommonSubsequence {
 
-Input : X = “abcdxyz”, y = “xyzabcd” 
-Output : 4 
-Explanation:
-The longest common substring is “abcd” and is of length 4.
-*/
-
-/*
-Let m and n be the lengths of the first and second strings respectively.
-
-A simple solution is to one by one consider all substrings of the first string and for every substring check if it is a substring in the second string. Keep track of the maximum length substring. There will be O(m^2) substrings and we can find whether a string is substring on another string in O(n) time (See this). So overall time complexity of this method would be O(n * m2)
-
-Dynamic Programming can be used to find the longest common substring in O(m*n) time. The idea is to find the length of the longest common suffix for all substrings of both strings and store these lengths in a table. 
-*/
-
-class LongestCommonSubstring {
-	static int lcs(char s1[], char s2[], int m, int n){
-		int LCStuff[][] = new int[m + 1][n + 1];
-	
-		// To store length of the longest
-		// common substring
-		int result = 0;
-
-		// Following steps build
-		// LCSuff[m+1][n+1] in bottom up fashion
-		for (int i = 0; i <= m; i++){
-			for (int j = 0; j <= n; j++){
-				if (i == 0 || j == 0){
-					LCStuff[i][j] = 0;
-                }
-				else if (s1[i - 1] == s2[j - 1]){
-					LCStuff[i][j] = LCStuff[i - 1][j - 1] + 1;
-					result = Integer.max(result, LCStuff[i][j]);
-				}
-				else{
-					LCStuff[i][j] = 0;
-                }
-			}
-		}
-		return result;
+	/* Returns length of LCS for X[0..m-1], Y[0..n-1] */
+	int lcs(char[] X, char[] Y, int m, int n)
+	{
+		if (m == 0 || n == 0)
+			return 0;
+		if (X[m - 1] == Y[n - 1])
+			return 1 + lcs(X, Y, m - 1, n - 1);
+		else
+			return max(lcs(X, Y, m, n - 1), lcs(X, Y, m - 1, n));
 	}
 
-	// Driver Code
-	public static void main(String[] args){
-		String s1 = "zxabcdezy";
-		String s2 = "yzabcdezx";
+	/* Utility function to get max of 2 integers */
+	int max(int a, int b)
+	{
+		return (a > b) ? a : b;
+	}
 
-		int m = s1.length();
-		int n = s2.length();
+	public static void main(String[] args)
+	{
+		LongestCommonSubsequence lcs = new LongestCommonSubsequence();
+		String s1 = "AGGTAB";
+		String s2 = "GXTXAYB";
 
-		System.out.println(lcs(s1.toCharArray(), s2.toCharArray(), m, n));
+		char[] X = s1.toCharArray();
+		char[] Y = s2.toCharArray();
+		int m = X.length;
+		int n = Y.length;
+
+		System.out.println("Length of LCS is"
+						+ " " + lcs.lcs(X, Y, m, n));
 	}
 }
+
+// Rishita Mitra
