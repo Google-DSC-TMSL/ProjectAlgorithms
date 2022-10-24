3867461f0ef127256438d0ce47013b115f285cda
pritam-dash <pritamdash84@gmail.com>
2022-10-24 14:57:03 +0530

    Checks Palindrome Word

---
+++ codes/java/PalindromeWord.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+/**
+ * Checks whether a word is Palindrome or not
+ *
+ * @author Pritam Dash
+ */
+class PalindromeWord
+{
+    /**
+     * Main method
+     *
+     * @param args Command line arguments
+     */
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String s=sc.nextLine();
+        int len=s.length();
+        String s1="",s2="";
+        for(int i=0, j=len-1; i<len/2; i++, j--)
+        {
+            s1+=s.charAt(i);
+            s2+=s.charAt(j);
+        }
+        if(s1.equalsIgnoreCase(s2))
+        System.out.println("Palindrome word");
+        else
+        System.out.println("Not Palindrome word");;
+    }
+}        
