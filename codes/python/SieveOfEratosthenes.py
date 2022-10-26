6ee9e4b4f5a78709104cccbb712533d674aa2891
Anushka Pathak <91817903+Anuroh619@users.noreply.github.com>
2022-10-26 12:21:53 +0530

    Create SieveOfEratosthenes.py

---
+++ codes/python/SieveOfEratosthenes.py
@@ -0,0 +1,11 @@
+#Print Prime numbers less than given number
+
+def prime_eratosthenes(n):
+    prime_list = []
+    for i in range(2, n+1):
+        if i not in prime_list:
+            print (i)
+            for j in range(i*i, n+1, i):
+                prime_list.append(j)
+
+prime_eratosthenes(150)
