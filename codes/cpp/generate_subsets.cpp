3a84a9ca7f3a7b102a49f81106e371906b0aa2e3
VETLA PAVAN KALYAN <20131a4463@gmail.com>
2022-10-28 16:12:43 +0530

    Update generate_subsets.cpp

--- codes/cpp/generate_subsets.cpp
+++ codes/cpp/generate_subsets.cpp
@@ -1,3 +1,23 @@
+/*
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+
+*/
+
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -25,11 +45,6 @@ void generate(vector<int> &subset, int i, vector<int> &nums)
 }
 
 int main() {
-	// your code goes here
-	// make input and output faster
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
 	int n;
 	cin>>n;
 	vector<int> nums(n), subset;
