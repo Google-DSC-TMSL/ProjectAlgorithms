c8cc3131674d87af89aa1ea0bb8a6eebf19f4f0a
VETLA PAVAN KALYAN <20131a4463@gmail.com>
2022-10-28 16:10:48 +0530

    Update generate_subsets_bit_masking.cpp

--- codes/cpp/generate_subsets_bit_masking.cpp
+++ codes/cpp/generate_subsets_bit_masking.cpp
@@ -1,7 +1,25 @@
+/* Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order. 
+
+Example-1:
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+Example-2:
+Input: nums = [0]
+Output: [[],[0]]
+
+*/
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 /* Time Complexity = (N * 2^N) */
+
+// Solution is this below fucntion
 
 vector<vector<int>> subsets(vector<int>& nums) {
 	int n = nums.size();
