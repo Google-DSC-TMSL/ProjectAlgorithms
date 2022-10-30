1e95c92f4f507723c55fe281bfb1c11380e8fa59
Rishita <85801090+RishitaMitra@users.noreply.github.com>
2022-10-30 21:48:01 +0530

    update interleaving string by join & zip function
    I update the code using join() and zip() functions for interleaving string.

--- codes/python/InterleavingStringsProg.py
+++ codes/python/InterleavingStringsProg.py
@@ -1,27 +1,18 @@
-# INTERLEAVING STRINGS( LEETCODE 97) SOLUTION 
-# Question : https://leetcode.com/problems/interleaving-string/
-
-#--------------------------------------------------------------------------
-
-class Solution:
-    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        m, n = len(s1), len(s2)
-        if m + n != len(s3):
-            return False
-        dp = [[False] * (n + 1) for _ in range(m + 1)]
-        dp[0][0] = True
-        for i in range(1, m + 1):
-            dp[i][0] = dp[i - 1][0] and s1[i - 1] == s3[i - 1]
-        for j in range(1, n + 1):
-            dp[0][j] = dp[0][j - 1] and s2[j - 1] == s3[j - 1]
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                choose_s1, choose_s2 = False, False
-                if s1[i - 1] == s3[i + j - 1]:
-                    choose_s1 = dp[i - 1][j]
-                if s2[j - 1] == s3[i + j - 1]:
-                    choose_s2 = dp[i][j - 1]
-                dp[i][j] = choose_s1 or choose_s2
-
-        return dp[m][n]
-        
+# Python3 code to demonstrate
+# Interleaving two strings
+# using join() + zip()
+  
+# initializing strings 
+test_string1 = 'mathematics'
+test_string2 = 'computerfreak'
+  
+# printing original strings  
+print("The original string 1 : " + test_string1)
+print("The original string 2 : " + test_string2)
+  
+# using join() + zip()
+# Interleaving two strings
+res = "".join(i + j for i, j in zip(test_string1, test_string2))
+      
+# print result
+print("The Interleaved string : " + str(res))
